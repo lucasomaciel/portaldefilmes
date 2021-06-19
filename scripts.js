8109c4148fd7c68dbe10a833176fee29c2ff5d7b
lucasomaciel <75627101+lucasomaciel@users.noreply.github.com>
2021-06-18 21:16:27 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,46 @@
+const TMDB_ENDPOINT = "https://api.themoviedb.org/3";
+const APIKEY = "4847565d3f9ebfae090d7a6b85608fa5";
+const IMG_PREFIX = "https://image.tmdb.org/t/p/w500";
+let xhr;
+
+function carregaFilmes () {
+    xhr = new XMLHttpRequest ();
+
+    xhr.open ('GET', TMDB_ENDPOINT + '/movie/popular' + '?api_key=' + APIKEY, true);
+    xhr.onload = exibeFilmes;
+    xhr.send();
+}
+
+function pesquisaFilmes () {
+    xhr = new XMLHttpRequest ();
+
+    query = document.getElementById('pesquisa').value;
+
+    xhr.open ('GET', TMDB_ENDPOINT + '/search/movie' + '?api_key=' + APIKEY + '&query=' + query, true);
+    xhr.onload = exibeFilmes;
+    xhr.send();
+}
+
+
+function exibeFilmes (evt) {
+    
+    let data = JSON.parse (xhr.responseText);
+    let textoHTML = '';
+
+    for (let i = 0; i < 3; i++) {
+        let nomeFilme = data.results[i].title;
+        let sinopse = data.results[i].overview;
+        let imagem = IMG_PREFIX + data.results[i].poster_path;
+
+        textoHTML += `<div class="card col-md-4">
+            <img src="${imagem}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${nomeFilme}</h5>
+                <p class="card-text">${sinopse}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>`
+    }
+
+    document.getElementById('tela').innerHTML = textoHTML;
+}
